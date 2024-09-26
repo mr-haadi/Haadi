1c5bc1f3eb65799dbacd77e06e54b1e90e5f959d
Mir Haadi <saifraja1611@gmail.com>
2024-09-26 17:43:11 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then((res) => {
+        console.log(res.data);
+    })
+
+
+console.log("Program Executed!");
